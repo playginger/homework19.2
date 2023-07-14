1b37215ac9ed0bfa408d3f1eaecfd3192fca260f
Серж Рыж (playginger) <7playginger7@gmail.com>
2023-07-14 20:53:42 +0300

    Домашка 20.1

--- catalog/management/commands/fill.py
+++ catalog/management/commands/fill.py
@@ -1,24 +1,36 @@
 from django.core.management import BaseCommand
 
-from catalog.models import Product
+from catalog.models import Product, Category
 
 
 class Command(BaseCommand):
 
     def handle(self, *args, **options):
+        category_create = [{
+            'category_name': 'железо ПК', 'category_description': 'комплектующие'},
+            {'category_name': 'Допы к ПК', 'category_description': 'всякое'
+
+             }]
+
+        category_for_create = []
+        for category in category_create:
+            category_for_create.append(Category(**category))
+
+        Category.objects.bulk_create(category_for_create)
+
         product_create = [
             {'product_name': 'Cистемные блоки', 'product_description': 'Игровые, мощные, бомбические ПК',
-             'category': 2, 'product_prise': 123000},
+             'category': category_for_create[1], 'product_prise': 123000},
             {'product_name': 'Звук', 'product_description': 'Крутая стереосистема, соседи будут не рады',
-             'category': 1, 'product_prise': 50000},
+             'category': category_for_create[0], 'product_prise': 50000},
             {'product_name': 'Микрофоны', 'product_description': 'Для качественного стриминга, нужно пара микрофонов',
-             'category': 1, 'product_prise': 7000},
+             'category': category_for_create[0], 'product_prise': 7000},
             {'product_name': 'Мониторы', 'product_description': 'Что бы видеть каждую морщинку, каждый пиксель,'
                                                                 ' каждую пищинку, ну вы поняли ',
-             'category': 2, 'product_prise': 15000},
+             'category': category_for_create[1], 'product_prise': 15000},
             {'product_name': 'Клавиатура и мышь', 'product_description': 'Что бы ваши пальчики были в тонусе,'
                                                                          ' и в сложных катках вы были MVP',
-             'category': 2, 'product_prise': 5000}
+             'category': category_for_create[1], 'product_prise': 5000}
         ]
 
         product_for_create = []
