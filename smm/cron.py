0048a38a2a177024518456680dd16a3ab4de28ea
Серж Рыж (playginger) <7playginger7@gmail.com>
2023-08-18 20:51:35 +0300

    Курсач Django

--- smm/cron.py
+++ smm/cron.py
@@ -1,7 +1,7 @@
-# from apscheduler.schedulers.background import BackgroundScheduler
-# from datetime import datetime
-#
-#
+from apscheduler.schedulers.background import BackgroundScheduler
+from datetime import datetime, timedelta
+
+
 # def my_task():
 #     scheduler = BackgroundScheduler()
 #     scheduler.add_job(my_task, 'interval', hourse=24)
@@ -10,14 +10,52 @@
 #     print("Задача выполняется ", datetime.now())
 #
 #     scheduler.shutdown()  # остановка задачи
-#
 
-# import winioctlcon
-# import os
-#
-# drive = 'C:'
-# handle = os.open(drive, os.O_RDONLY | os.O_BINARY)
-# ioctl = winioctlcon.IOCTL_DISK_GET_LENGTH_INFO
-# buf = winioctlcon.STORAGE_DEVICE_DESCRIPTOR()
-# winioctlcon.DeviceIoControl(handle, ioctl, None, 0, buf, len(buf))
-# os.close(handle)
+def my_task():
+    print("Задача выполняется", datetime.now())
+
+
+def add_job(send_time, interval, status):
+    # Проверка валидности аргументов
+    if interval.lower() not in ["день", "неделя", "месяц"]:
+        print("Ошибка: Неверная периодичность. Допустимые значения: день, неделя, месяц.")
+        return
+
+    if status.lower() not in ["завершена", "создана", "запущена"]:
+        print("Ошибка: Неверный статус рассылки. Допустимые значения: завершена, создана, запущена.")
+        return
+
+    # Определяем временной интервал выполнения задачи
+    if interval.lower() == "день":
+        delta = timedelta(days=1)
+    elif interval.lower() == "неделя":
+        delta = timedelta(weeks=1)
+    elif interval.lower() == "месяц":
+        delta = timedelta(days=30)  # Приблизительное значение для месяца
+
+    next_send_time = datetime.now().replace(hour=send_time.hour, minute=send_time.minute, second=send_time.second)
+    while next_send_time < datetime.now():
+        next_send_time += delta
+
+    # Запускаем задачу с указанными параметрами
+    scheduler.add_job(my_task, 'date', run_date=next_send_time)
+    print(
+        f"Задача расписания добавлена: Время рассылки - {next_send_time}, Периодичность - {interval}, Статус - {status}")
+
+
+scheduler = BackgroundScheduler()
+
+try:
+    send_time = datetime.now() + timedelta(seconds=10)  # Предполагаемое время первой рассылки (10 секунд для примера)
+    interval = "день"  # Периодичность рассылки
+    status = "создана"  # Статус рассылки
+
+    add_job(send_time, interval, status)
+    scheduler.start()  # Запуск задачи
+
+    while True:
+        pass  # Добавьте свой код здесь, если требуется
+
+except KeyboardInterrupt:
+    print("Остановка задачи...")
+    scheduler.shutdown()  # Останавливаем задачу перед выходом
