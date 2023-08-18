cff0132202fb4252b8754cec019feed523fe533f
Серж Рыж (playginger) <7playginger7@gmail.com>
2023-08-18 20:23:04 +0300

    Курсач

--- catalog/management/commands/fill2.py
+++ catalog/management/commands/fill2.py
@@ -1,20 +1,19 @@
 from time import sleep
-
-from django.core.management import BaseCommand
-
-from catalog.models import Product, Category
+from django.core.management.base import BaseCommand
 from smm.models import Newsletter
+from datetime import datetime, timedelta
 
 
 class Command(BaseCommand):
-
     def handle(self, *args, **options):
         news = Newsletter.objects.all()
-
+        today = datetime.now().date()
         for new in news:
             last = new.logs.all().order_by('-last_attempt').first()
-            if  last.last_attempt + new.frequency > today():
-                send()
-
+            if last and last.last_attempt.date() + timedelta(days=new.frequency) > today:
+                send(new)
             sleep(5)
 
+
+def send(newsletter):
+    print(f"Отправка рассылки для {newsletter}")
